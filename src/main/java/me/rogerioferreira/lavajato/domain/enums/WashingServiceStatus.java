c453d1f12e35338cd1a663f229f5e0f04878e8ff
Rogério Ferreira <rogersanctus@gmail.com>
2025-04-08 00:45:05 -0300

    Adiciona base para validação de transição de Status na enum WashingServiceStatus e adiciona métodos e anotações para Serialização e Desserialização

--- src/main/java/me/rogerioferreira/lavajato/domain/enums/WashingServiceStatus.java
+++ src/main/java/me/rogerioferreira/lavajato/domain/enums/WashingServiceStatus.java
@@ -1,8 +1,51 @@
 package me.rogerioferreira.lavajato.domain.enums;
 
+import java.util.Collections;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import me.rogerioferreira.lavajato.domain.exceptions.InvalidLabelForEnumException;
+
 public enum WashingServiceStatus {
-  WAITING,
-  IN_PROGRESS,
-  FINISHED,
-  CANCELED
+  WAITING("waiting", List.of("in_progress", "canceled")),
+  IN_PROGRESS("in_progress", List.of("finished", "canceled")),
+  FINISHED("finished", Collections.emptyList()),
+  CANCELED("canceled", Collections.emptyList());
+
+  public final String label;
+  public final List<String> nextStatuses;
+
+  WashingServiceStatus(String label, List<String> nextStatuses) {
+    this.label = label;
+    this.nextStatuses = nextStatuses;
+  }
+
+  @JsonValue
+  public String getLabel() {
+    return this.label;
+  }
+
+  @JsonCreator
+  public static WashingServiceStatus fromLabel(String label) throws InvalidLabelForEnumException {
+    return List.of(WashingServiceStatus.values())
+        .stream()
+        .filter(status -> status.label.equals(label))
+        .findFirst()
+        .orElseThrow(() -> new InvalidLabelForEnumException(WashingServiceStatus.class, label,
+            WashingServiceStatus.getLabels()));
+  }
+
+  public static List<String> getLabels() {
+    return List.of(WashingServiceStatus.values())
+        .stream()
+        .map(WashingServiceStatus::getLabel)
+        .toList();
+  }
+
+  @Override
+  public String toString() {
+    return this.label;
+  }
 }
